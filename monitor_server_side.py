8b773c2876da908748a6b10c54f00de9fbd757d9
OTMAN EL MABROUK <84495389+otman-elmabrouk@users.noreply.github.com>
2022-05-11 23:04:22 +0100

    Update monitor_server_side.py

--- monitor_server_side.py
+++ monitor_server_side.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+HOST="any thing"
+PORT=5555
 import os
 i=1
 dir_exist = os.path.isdir("./transfers")
 if dir_exist == False:
         os.mkdir("./transfers") 
 while True:
-        command = "nc -lvp 4444 > ./transfers/docs" +str(i)
+        command = "nc -lvp"+ str(PORT) +" > ./transfers/docs" +str(i)
         os.system(command)
         i += 1
